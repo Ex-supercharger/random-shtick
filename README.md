ef69f58646eb9b02788efc4d98bb56952e11afa1
Ex-supercharger <93357754+Ex-supercharger@users.noreply.github.com>
2025-12-31 19:04:32 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Random Shtick
+A minecraft mod with a whole bunch of random stuff for the sake of having it and testing I guess
---
+++ src/client/java/io/github/ex_supercharger/random_shtick/Random_ShtickClient.java
@@ -0,0 +1,10 @@
+package io.github.ex_supercharger.random_shtick;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Random_ShtickClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
